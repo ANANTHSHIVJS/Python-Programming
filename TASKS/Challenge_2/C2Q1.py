ebe03709cdd49e766747aa8821c788ee4ba60b5e
Ananth Shiv JS <115144803+ANANTHSHIVJS@users.noreply.github.com>
2025-03-04 13:17:28 +0400

    Add files via upload

---
+++ TASKS/Challenge_2/C2Q1.py
@@ -0,0 +1,14 @@
+ ## Using a for-loop,
+ ## Calculates the addition of each number
+ ## Eg:
+ ## INPUT
+ ## X= [10,20,30,40]
+ ## OUTPUT
+ ## 20
+ ## 40
+ ## 60
+ ## 80
+
+num=[7,10,22,18]
+print(sum(num))
+
---
+++ TASKS/Challenge_2/C2Q2.py
@@ -0,0 +1,6 @@
+#Print “I Love My India" 7 times.
+
+i=7
+for i in range(7):
+    print("I love my INDIA")
+    i+=1
