335540372efb65e77257fe22156ff79ddd76e03a
Ananth Shiv JS <115144803+ANANTHSHIVJS@users.noreply.github.com>
2025-03-05 23:30:20 +0400

    Add files via upload

---
+++ TASKS/Challenge_6/C6Q1.py
@@ -0,0 +1,7 @@
+# Write Python code which Accept the student name and in turn
+# returns their respective Mark. Make sure to use dictionaries to store
+# student name and their respective mark. The dictionary should
+# include at least 7 elements.
+
+sheet={"Shiva":86, "Meghu":95, "Adhi":70,"Lachu":85,"Sree":60,"Paaru":75,"Ashwin":59,"Kicha":78}
+print(sheet)
