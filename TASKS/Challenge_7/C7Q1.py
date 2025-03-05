e1b1ff6d574ef5e676baa24c2ecef4c4e14997b9
Ananth Shiv JS <115144803+ANANTHSHIVJS@users.noreply.github.com>
2025-03-05 23:31:04 +0400

    Add files via upload

---
+++ TASKS/Challenge_7/C7Q1.py
@@ -0,0 +1,6 @@
+# List out all the even numbers from 2 to 222 using lists in Python
+
+list=[]
+for i in range(2,224,2):
+    list.append(i)
+print(list)
---
+++ TASKS/Challenge_7/C7Q2.py
@@ -0,0 +1,6 @@
+# List out all the odd numbers from 3 to 99 using lists in Python.
+
+odd=[]
+for i in range(3,100,2):
+    odd.append(i)
+print(odd)
