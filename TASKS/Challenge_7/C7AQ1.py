a04138f301bb25f482b9edc6b4cf54cadcdcfe20
Ananth Shiv JS <115144803+ANANTHSHIVJS@users.noreply.github.com>
2025-03-06 11:41:14 +0400

    Add files via upload

---
+++ TASKS/Challenge_7/C7AQ1.py
@@ -0,0 +1,7 @@
+# .Create a new list from existing list (Use String Format)
+
+l=["Shiva"]
+m=l
+m.append("is")
+m.append("live")
+print(m)
---
+++ TASKS/Challenge_7/C7AQ2.py
@@ -0,0 +1,6 @@
+# Create a List with 8 elements
+# Print second index to sixth index.
+
+elements=["a","b","c","d","e","f","g","h"]
+for i in range(2,7):
+    print(elements[i])
